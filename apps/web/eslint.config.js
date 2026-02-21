3d26b0a9c64cfafead5fadc5e54538706a1029d9
Noorain Panjwani <noorain.panjwani@gmail.com>
2026-02-20 16:12:11 -0800

    Configured my lint and format commands
    Signed-off-by: Noorain Panjwani <noorain.panjwani@gmail.com>

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,23 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from "eslint/config";
+import {
+  baseRules,
+  commonIgnores,
+  createLanguageOptions,
+  plugins,
+  prettierConfig,
+  tsFiles,
+  browserGlobals,
+} from "@isagi/tooling/eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  prettierConfig,
+  globalIgnores([...commonIgnores, "dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: tsFiles,
+    plugins: {
+      ...plugins,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...baseRules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ...createLanguageOptions(import.meta.dirname),
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+      },
+      globals: {
+        ...createLanguageOptions(import.meta.dirname)?.globals,
+        ...browserGlobals,
+      },
     },
   },
-])
+]);
