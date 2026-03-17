2a3878d84c2ea1b372a58819f750741c85d581c8
Noorain Panjwani <noorain.panjwani@gmail.com>
2026-03-17 11:18:13 -0700

    chore: Removed sparks page from routes
    Signed-off-by: Noorain Panjwani <noorain.panjwani@gmail.com>

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/project/ProjectDetail";
 import Session from "./pages/session/Session";
-import Sparks from "./pages/Sparks";
+// import Sparks from "./pages/Sparks";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sparks" element={<Sparks />} />
+      {/* <Route path="/sparks" element={<Sparks />} /> */}
       <Route path="/session/:id" element={<Session />} />
       <Route path="/project/:id" element={<ProjectDetail />} />
     </Routes>
